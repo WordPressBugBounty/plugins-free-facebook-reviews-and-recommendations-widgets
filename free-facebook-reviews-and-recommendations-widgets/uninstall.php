5d67ded4785aef4d0b6f02cf84857fc955e240ab
Sean Murphy <sean@iamseanmurphy.com>
2025-07-24 11:17:22 +0000

    13.0

--- free-facebook-reviews-and-recommendations-widgets/uninstall.php
+++ free-facebook-reviews-and-recommendations-widgets/uninstall.php
@@ -3,6 +3,6 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 die;
 }
 require_once plugin_dir_path( __FILE__ ) . 'trustindex-plugin.class.php';
-$trustindex_pm_facebook = new TrustindexPlugin_facebook("facebook", __FILE__, "12.9", "Widgets for Reviews & Recommendations", "Facebook");
+$trustindex_pm_facebook = new TrustindexPlugin_facebook("facebook", __FILE__, "13.0", "Widgets for Reviews & Recommendations", "Facebook");
 $trustindex_pm_facebook->uninstall();
 ?>
